3d6562494893c4ab14fa9f6c3d4d096430d2daba
Dave Strus <dave@getfretless.com>
2016-06-02 11:08:23 -0400

    Add the student to the list.

--- js/app.js
+++ js/app.js
@@ -6,11 +6,16 @@ var megaRoster = {
   },
 
   setupEventListeners: function() {
-    document.querySelector('#studentForm').onsubmit = this.addStudent;
+    document.querySelector('#studentForm').onsubmit = this.addStudent.bind(this);
   },
 
   addStudent: function(ev) {
     ev.preventDefault();
+    var f = ev.currentTarget;
+    var studentName = f.studentName.value;
+    var item = this.buildListItem(studentName);
+    var list = document.querySelector('#studentList');
+    list.appendChild(item);
   },
 
   buildListItem: function(studentName) {
