41ffaac0c53743d4c154ca3c2811b74837362d4c
Dave Strus <dave@getfretless.com>
2016-06-02 10:18:23 -0400

    Set up event handler for form submission.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
 $(document).foundation()
+
+var megaRoster = {
+  init: function() {
+    this.setupEventListeners();
+  },
+
+  setupEventListeners: function() {
+    document.querySelector('form').onsubmit = this.addStudent;
+  },
+
+  addStudent: function(ev) {
+    ev.preventDefault();
+    console.log('hello world');
+  },
+
+}
+
+megaRoster.init();
